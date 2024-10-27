ce6c9e745dc7ca76dd979782c95e7a7790d7adbf
Noulakl <andriatsaranolalaina@gmail.com>
2024-10-27 13:25:14 +0300

    some adjustment

---
+++ index.js
@@ -0,0 +1,13 @@
+// Grabbing Element
+const inputValueElement = document.getElementById("pxValueInput")
+const remDisplay = document.getElementById("rem")
+
+// the magics
+inputValueElement.addEventListener("input",function(){
+    const defaultPxValue = document.getElementById("defaultPxValue").value
+    const inputValue = document.getElementById("pxValueInput").value
+
+    const calculatedRem = inputValue / defaultPxValue
+    remDisplay.textContent = `${calculatedRem.toFixed(3)}rem`
+    navigator.clipboard.writeText(`${remDisplay.textContent}`)
+})
